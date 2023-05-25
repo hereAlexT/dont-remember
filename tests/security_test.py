b2ec0f23c5b8c4813bab7a3fd0a500d20e3755c5
Cherian <51251921+cherianjohn@users.noreply.github.com>
2023-05-25 06:04:17 +0000

    Extended security test for every word function

--- tests/security_test.py
+++ tests/security_test.py
@@ -3,7 +3,6 @@ import requests
 
 pytest.localhost_user = 'http://127.0.0.1:8888/api/v1/'
 pytest.localhost_word = 'http://127.0.0.1:8889/api/v1/'
-pytest.token = ''
 
 @pytest.fixture(scope='session', autouse=True)
 def api_token():
@@ -19,7 +18,7 @@ def api_token():
         
         # Check if the login request was successful
         if response.status_code == 404:
-            signup_response = request.post(pytest.localhost_user + 'signup', json=data)
+            signup_response = requests.post(pytest.localhost_user + 'signup', json=data)
             assert signup_response.status_code == 200, f"Signup request failed with status code {signup_response.status_code}"
 
             response = requests.post(login_url, json=data)
@@ -43,7 +42,7 @@ def test_add_new_word(api_token):
         'word': 'ahorse'
     }
     response = requests.post(api_url, headers=headers, json=payload)
-    print(response.status_code)
+    print(api_url, response.status_code)
     # Check if the request was successful
     assert response.status_code != 401, "API request failed with status code 401 (Unauthorized)"
     
@@ -54,8 +53,7 @@ def test_next_word(api_token):
         'Authorization': f'Bearer {api_token}'
     }
     response = requests.get(api_url, headers=headers)
-    print(response.status_code)
-    print(api_url)
+    print(api_url, response.status_code)    
     # Check if the request was successful
     assert response.status_code != 401, "API request failed with status code 401 (Unauthorized)"
 
@@ -65,8 +63,12 @@ def test_update_word(api_token):
     headers = {
         'Authorization': f'Bearer {api_token}'
     }
-    response = requests.post(api_url, headers=headers)
-    print(response.status_code)
+    payload = {
+        'word': 'ahorse',
+        'result': 'forget'
+    }
+    response = requests.post(api_url, headers=headers, json=payload)
+    print(api_url, response.status_code)
     # Check if the request was successful
     assert response.status_code != 401, "API request failed with status code 401 (Unauthorized)"
 
@@ -77,7 +79,7 @@ def test_word_history(api_token):
         'Authorization': f'Bearer {api_token}'
     }
     response = requests.post(api_url, headers=headers)
-    print(response.status_code)
+    print(api_url, response.status_code)
     # Check if the request was successful
     assert response.status_code != 401, "API request failed with status code 401 (Unauthorized)"
 
@@ -88,7 +90,40 @@ def test_add_new_word_without_token():
         'word': 'ahorse'
     }
     response = requests.post(api_url, json=payload)
+    print(api_url, response.status_code)
+    # Check if the request returned a 401 status code (Unauthorized)
+    assert response.status_code == 401, "API request without token did not return status code 401"
+
+def test_next_word_without_token():
+    # Make a request to add a new word to the API without a token
+    api_url = pytest.localhost_word + 'next_word' 
+    response = requests.get(api_url)
+    print(api_url, response.status_code)
     
     # Check if the request returned a 401 status code (Unauthorized)
     assert response.status_code == 401, "API request without token did not return status code 401"
+
+def test_update_word_without_token():
+    # Make a request to add a new word to the API without a token
+    api_url = pytest.localhost_word + 'update_word' 
+    payload = {
+        'word': 'ahorse',
+        'result': 'forget'
+    }
+    response = requests.post(api_url, json=payload)
+    print(api_url, response.status_code)
+    
+    # Check if the request returned a 401 status code (Unauthorized)
+    assert response.status_code == 401, "API request without token did not return status code 401"
+
+def test_word_history_without_token():
+    # Make a request to add a new word to the API without a token
+    api_url = pytest.localhost_word + 'word_history' 
+    response = requests.get(api_url)
+    print(api_url, response.status_code)
+    
+    # Check if the request returned a 401 status code (Unauthorized)
+    assert response.status_code == 401, "API request without token did not return status code 401"
+
+def test_token_expiration
     
