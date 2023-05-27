69c4d8a73588e530ddcbe3db61d0846a3872f160
HOME\croby <croby99@gmail.com>
2023-05-27 10:18:45 +1000

    Fixed url errors in availability test

--- tests/test_availability.py
+++ tests/test_availability.py
@@ -7,11 +7,11 @@ def test_health_check_endpoint():
     
 
     for _ in range(24):  # 24 hours
-        response_1 = requests.get(word_url + '\health')
-        assert response_1.status_code == 200, f"Endpoint {endpoint_1} returned {response_1.status_code} status code."
+        response_1 = requests.get(word_url + '/health')
+        assert response_1.status_code == 200, f"Endpoint {word_url} returned {response_1.status_code} status code."
 
-        response_2 = requests.get(user_url + '\health')
-        assert response_2.status_code == 200, f"Endpoint {endpoint_2} returned {response_2.status_code} status code."
+        response_2 = requests.get(user_url + '/health')
+        assert response_2.status_code == 200, f"Endpoint {user_url} returned {response_2.status_code} status code."
 
         time.sleep(3600)  # Pause for 1 hour (3600 seconds) before the next check
 
